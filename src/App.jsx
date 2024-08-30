abebdaf6d96adbb26e1ab6234c500a4f8c83aa42
Suyel Bishnu <suyel2007@gmail.com>
2024-08-30 14:03:56 +0300

    Import storeNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import List from './components/List';
 import Cart from './components/Cart';
+import StoreNavbar from './components/StoreNavbar';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+      <StoreNavbar/>
         <Routes>
           <Route path="/" element={<List />} />
           <Route path="/cart" element={<Cart />} />
