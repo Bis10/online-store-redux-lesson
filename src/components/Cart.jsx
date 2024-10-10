73f25fe4f1e63ae2c6371bbbdb6d4fc04b3a2dce
Suyel Bishnu <suyel2007@gmail.com>
2024-10-10 13:05:42 +0300

    Update the return

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { useAppSelector } from "../hooks/hooks";
 import { Container, ListGroup } from "react-bootstrap";
+import Product from "./Product";
 
 const Cart = () => {
   const cartItems = useAppSelector((state) => state.products.cart);
 
+  //   return (
+  //     <Container className="p-3">
+  //       <h1 className="text-primary display-6 fw-bold">Cart</h1>
+  //       {cartItems.length === 0 ? (
+  //         <p>Your cart is empty.</p>
+  //       ) : (
+  //         <ListGroup>
+  //           {cartItems.map((item) => (
+  //             <ListGroup.Item key={item.id}>
+  //               {item.title} - ${item.price}
+  //             </ListGroup.Item>
+  //           ))}
+  //         </ListGroup>
+  //       )}
+  //     </Container>
+  //   );
+  // };
+
+  // export default Cart;
+
   return (
-    <Container className="p-3">
-      <h1 className="text-primary display-6 fw-bold">Cart</h1>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ListGroup>
-          {cartItems.map((item) => (
-            <ListGroup.Item key={item.id}>
-              {item.title} - ${item.price}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      )}
-    </Container>
+    <div>
+      Cart will be here
+      {cartItems.map((item, index) => (
+        <Product key={`${item.id}_${index}`} {...item} />
+      ))}
+    </div>
   );
 };
-
 export default Cart;
